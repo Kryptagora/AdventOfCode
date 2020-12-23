13d4afec851f7419cd386865fa91d37c18becb23
Kryptagora <amontesla@mailbox.org>
2020-12-23 18:33:51 +0100

    🎄 organized repo

--- build.py
+++ build.py
@@ -4,7 +4,7 @@ from os import popen
 from fire import Fire
 
 def build_day(day, year='2020'):
-    path = Path(str(year)) / str(int(day))
+    path = Path(str(year)) / ('day_' + str(int(day)).zfill(2))
     copytree('template', path)
 
     # Automatically opens directory in VSCode
